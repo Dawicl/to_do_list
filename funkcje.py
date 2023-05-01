21a425dc65ecb8a29bc330a220831f8cc725f94e
unknown <dawid.pawlak3720@gmail.com>
2023-05-01 17:38:29 +0200

    aktualizacja_dodawania_zadan

--- funkcje.py
+++ funkcje.py
@@ -1,34 +1,38 @@
 wybor_zadania = -1
 
 zadania = []
-terminy = []
-lista_zadan = zadania + terminy
-
 
 def pokaz_zadania():
     numer_zadania = 0
-    for zadanie in lista_zadan:
-        print(zadanie + " " + "[" + str(numer_zadania) + "]")
+    for zadanie in zadania:
+        print(zadanie[0] + " termin realizacji: " + zadanie[1] + " " + "[" + str(numer_zadania) + "]")
         numer_zadania += 1
 
 def dodaj_zadanie():
     zadanie = input("Podaj zadanie które chcesz dodać: ")
     termin = input("Podaj termin realizacji zadania: ")
-    zadania.append(zadanie)
-    terminy.append(termin)
-    lista_zadan.append(zadanie + " - termin realizacji: " + termin)
-    print("Zadanie zostało dodane do listy.")
+    if len(termin) == 10 and termin[0:2].isdigit() and termin[2] == '.' and termin[3:5].isdigit() \
+    and termin[5] == '.' and termin[5:-1].isdigit() and int(termin[0:2]) >= 0 and int(termin[0:2]) <= 31:
+        pojedyncze_zadanie = [zadanie, termin]
+        zadania.append(pojedyncze_zadanie)
+        print("Zadanie zostało dodane do listy.")
+    else:
+        print()
+        print("Podaj datę w formacie dd.mm.rrrr")
+        print("Podaj zadanie jeszcze raz \n --------------------")
+        dodaj_zadanie()
+
 
 def usun_zadanie():
     try:
         pokaz_zadania()
         print("")
         numer_zadania = int(input("Podaj numer zadania do usunięcia: "))
-        if numer_zadania < 0 or numer_zadania > len(lista_zadan) - 1:
+        if numer_zadania < 0 or numer_zadania > len(zadania) - 1:
             print("Brak zadania o podanym numerze")
             return
     
-        lista_zadan.pop(numer_zadania)
+        zadania.pop(numer_zadania)
         print("Wybrane zadanie zostało usunięte.")
     except:
         print("Podawaj tylko numery indeksów!")
@@ -36,7 +40,7 @@ def usun_zadanie():
 
 def zapisz_zadania():
     plik = open("lista_zadan.txt", "w")
-    for zadanie in lista_zadan:
+    for zadanie in zadania:
         plik.write(str(zadanie+"\n"))
     plik.close
 
@@ -44,7 +48,7 @@ def zaladuj_zadania():
     try:
         plik = open("lista_zadan.txt")
         for linia in plik.readlines():
-            lista_zadan.append(linia.strip())
+            zadania.append(linia.strip())
 
         plik.close
     except FileNotFoundError:
